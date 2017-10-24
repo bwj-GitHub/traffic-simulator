8d236dc53529918a1b1e0038198cc4a329833c7f
bwj-GitHub <bwj_ucf@knights.ucf.edu>
2017-10-24 16:11:48 -0400

    Added missing methods to TrafficQueue.

--- src/traffic/TrafficQueue.java
+++ src/traffic/TrafficQueue.java
@@ -1,5 +1,19 @@
 package traffic;
 
 public class TrafficQueue {
+	
+	// TODO: Write me!
+	
+	public void addCar(Car car) {
+		// TODO: Write me!
+	}
+
+	public boolean isEmpty() {
+		// TODO: Write me!
+	}
+	
+	public boolean isFull() {
+		// TODO: Write me!
+	}
 
 }
