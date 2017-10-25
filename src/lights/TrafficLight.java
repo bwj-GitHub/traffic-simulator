20a2963805845bbe71450cc93a2f5807c0478d9b
bwj-GitHub <bwj_ucf@knights.ucf.edu>
2017-10-25 19:36:55 -0400

    Implemented TrafficLight.

--- src/lights/TrafficLight.java
+++ src/lights/TrafficLight.java
@@ -1,14 +1,29 @@
 package lights;
 
+import events.carEvents.CarEvent;
+import events.lightEvents.LightEvent;
 import traffic.TrafficQueue;
 
 public class TrafficLight {
-	public boolean isGreen;
-	public boolean isRed;
-	public float nextChangeTime;
+	public LightColor color;
+	public float minTimeToNextChange; 
 	public TrafficQueue trafficQueue;
-	
+
 	public boolean isGreen() {
-		return isGreen;
+		// TODO: Once acceleration is implemented, YELLOW should no longer count
+		//  as GREEN.
+		if (color == LightColor.GREEN || color == LightColor.YELLOW) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public CarEvent[] updateLight(LightEvent event) {
+		if (event.color == LightColor.GREEN) {
+			return trafficQueue.updateCars(event);
+		} else {
+			return null;
+		}
 	}
 }
