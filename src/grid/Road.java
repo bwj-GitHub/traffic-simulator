92b2a7b0ceab566b4e35b299cf86e93136b6d2dc
bwj-GitHub <bwj_ucf@knights.ucf.edu>
2017-10-25 10:47:25 -0400

    Road handles new cars.

--- src/grid/Road.java
+++ src/grid/Road.java
@@ -1,8 +1,10 @@
 package grid;
 
-import events.Event;
 import events.carEvents.CarEvent;
+import events.carEvents.CarUpdateEvent;
+import lights.TrafficLight;
 import traffic.Car;
+import traffic.TrafficQueue;
 
 public class Road {
 	int index;
@@ -34,7 +36,46 @@ public class Road {
 		}
 	}
 
-	public CarEvent[] handleNewCar(Car car) {
-		// TODO: Write me!
+	/**
+	 * Update car's state to reflect its location at the beginning of this Road;
+	 * return the car's next Event(s).
+	 * 
+	 * @param car
+	 * @return CarEvent[]
+	 */
+	public CarEvent handleNewCar(Car car) {
+		// Determine car's next Intersection:
+		RoadSegment roadSegment = roadSegments[firstRoadSegmentIndex];
+		Intersection intersection = roadSegment.outIntersection;
+		int i = intersection.intersectionRowIndex;
+		int j = intersection.intersectionColIndex;
+
+		// Check if this first RoadSegment is full:
+		TrafficLight trafficLight = intersection.getTrafficLight(car.onAvenue);
+		TrafficQueue trafficQueue = trafficLight.trafficQueue;
+		if (trafficQueue.isFull()) {
+			// Place the car in the TrafficQueue -- it is okay for the first segment
+			//  to be over-full:
+			trafficQueue.addCar(car);
+
+			// Update car's state:
+			car.updateNextEvent(null);
+			return null;
+		}
+
+		// Calculate time to reach next Intersection (assuming there is no cars in
+		//  the TrafficQueue -- that will be handled later)
+		// Calculate the time that the next event will occur:
+		float travelDistance = roadSegment.length;
+		// TODO: Road needs config?
+		float travelTime = car.timeToDistance(travelDistance);
+		float nextTime = car.nextEvent.time() + travelTime;
+		CarUpdateEvent nextEvent = new CarUpdateEvent(car.id, i, j, nextTime);
+
+		// Update car's state:
+		car.setSegmentIndex(firstRoadSegmentIndex);
+		car.updateNextEvent(nextEvent);
+
+		return nextEvent;
 	}
 }
