62fa7a5fbbae8a1b4dde68243b9ffb0396f054c7
bwj-GitHub <bwj_ucf@knights.ucf.edu>
2017-10-25 19:36:28 -0400

    Implemented LightEvent.

--- src/events/lightEvents/LightEvent.java
+++ src/events/lightEvents/LightEvent.java
@@ -1,12 +1,18 @@
 package events.lightEvents;
 
 import events.Event;
+import lights.LightColor;
+import lights.TrafficLight;
 
 public class LightEvent extends Event {
+	
+	public TrafficLight light;
+	public LightColor color;
 
-	public LightEvent(float time) {
+	public LightEvent(float time, TrafficLight light, LightColor color) {
 		super(time);
-		// TODO Auto-generated constructor stub
+		this.light = light;
+		this.color = color;
 	}
 
 }
