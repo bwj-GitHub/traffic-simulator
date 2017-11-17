030055fda2a3e7d75f9e7a27c540dafbddd67d98
praveenpatchala <32908094+praveenpatchala@users.noreply.github.com>
2017-11-16 21:03:57 -0500

    Added code for forwarding multiple cars during carupdate event for convoys

--- src/grid/TrafficGrid.java
+++ src/grid/TrafficGrid.java
@@ -20,88 +20,119 @@ public class TrafficGrid  {
 	public TrafficGrid(Config config)
 	{
 		this.config=config;
-		numavenues = config.numrows;
-		numstreets = config.numcol;
+		numavenues=config.numrows;
+		numstreets=config.numcol;
+		int i,j;
 		intersections = new Intersection[numavenues][numstreets];
-		for (int i = 0; i < numavenues; i++)
-		{
-			for(int j = 0; j < numstreets; j++)
-			{
-				// BJ: why do I need numavenues and numstreets if I have config?
-				intersections[i][j] = new Intersection(i, j, numavenues, numstreets, config);
-			}
-		}
+		for (i=0;i<numavenues;i++)
+			for(j=0;j<numstreets;j++)
+				intersections[i][j]=new Intersection(i,j,numavenues,numstreets,config);
 	}
 
 	
-	public Intersection  getIntersection(int i, int j)
+	public Intersection  getIntersection(int i,int j)
 	{
 		return intersections[i][j];
 	}
 
-	// BJ: Why is this being passed the EventQueue?
-	public ArrayList<Event> handleCarUpdateEvent(CarUpdateEvent event, int currenttime, EventQueue eq)
+	
+	
+	
+	
+	public ArrayList<Event> handleCarUpdateEvent(CarUpdateEvent event,int currenttime,EventQueue eq,int algorithm)
 	{
 	//	System.out.println("We are inside car update event with time:"+ currenttime);
-
+		
 		int n; //Number of cars ahead it at lane.
-		ArrayList<Event> futureEvents = new ArrayList<Event>();
-		Car car = event.getCar();
+		ArrayList<Event> futureEvents=new ArrayList<Event>();
+		Car c=event.getCar();
 	//	System.out.println("Car with id:"+c.getId()+" has reached intersection and we are handling its car update event at time :"+ currenttime);
 	//	System.out.println("Intersection is:");
 	//	c.getCurrentLight().printpos();
 	//	System.out.println("");
 	//	System.out.println("The current light color is: "+c.getCurrentLight().getCurrentLightColor()+" Remaining light time is :"+c.getCurrentLight().getRemainingTime());
-		car.rest(); // Setting car to rest state.  // BJ: What is this?
-		int n1=car.getPosition();
+		c.rest(); // Setting car to rest state.
+		int n1=c.getCurrentLight().getcurrentposition(c);
 		int distance;
 		int time;
-		TrafficLight currentlight=car.getCurrentLight();
-
+		TrafficLight currentlight=c.getCurrentLight();
+		
+		
+		
 		//For handling self managed scheduling we add the following code which increments the counter in current traffic light & checks if counter exceeds threshold.
 		currentlight.incrementCounter();
-
-		if(car.getCurrentLight().getCurrentLightColor()==LightColor.green && car.getPosition()==1)
+		
+		
+		if(c.getCurrentLight().getCurrentLightColor()==LightColor.green && n1==1)
 		{
-			// Since trafficlight is green/yellow , We check if it can move to next traffic light and move it accordingly.
-			/*switch(c.getCurrentLane()) // Get the current position of car in respective lane.
+			
+			
+			if(algorithm==4)
 			{
-			case left: n=c.getCurrentLight().getLeftLane().indexOf(c);
-				break;
-			case middle: n=c.getCurrentLight().getMiddleLane().indexOf(c);
-				break;
-			case right: n=c.getCurrentLight().getRightLane().indexOf(c);
-				break;
-			}*/
-			// Get remaining light time and determine if within that time it can cross this intersection.
-		//	n1--;
-			//distance=n1*(c.getCarSize()+c.getCarSpacing());
-			//time= distance/c.getCarSpeed();
-			//if(time<=c.getCurrentLight().getRemainingTime())
-			//{
-				// We can proceed to move this car to next intersection.
-			// BJ: This if statement is too long
-				if (car.getNextLightIndex() == car.path.size() ||
-						(car.getCurrentLane() == Lane.middle &&
-						car.path.get(car.getNextLightIndex()).getMiddleLaneSize() <= car.getCurrentLight().getLaneLimit()) ||
-						(car.getCurrentLane()!=Lane.middle &&
-						(car.path.get(car.getNextLightIndex()).getNumberOfTurningCars() <= car.getCurrentLight().getLaneLimit())))
-				{	 
-				int check = car.getCurrentLight().removecar(car,currenttime); // We removed car and added it to appropriate traffic light.
-				// Now we need to create new carupdate event.
-				car.moving(); // Car is in motion.
-				if(check==1)
-				futureEvents.add(car.generateCarUpdateEvent(currenttime));
+			//We need to extend time if needed.
+				ArrayList<Car> middlelane=c.getCurrentLight().getMiddleLane();
+				int numcars=((c.getCurrentLight().getRemainingTime()*config.carspeed)+config.carspacing)/(config.carsize+config.carspacing);
+				int num2=0;
+				for(Car car:middlelane)
+				{
+					if(car.getState()==0)
+						num2++;
 				}
+				if(num2>numcars)
+				{
+					int newtime;
+					int x=((num2*(config.carsize+config.carspacing))-config.carspacing);
+					if(x%config.carspeed==0)
+						newtime=x/config.carspeed;
+					else
+						newtime=(x/config.carspeed)+1;
+					//Extending current light time.
+					c.getCurrentLight().setremaininglighttime(newtime);
+					c.getCurrentLight().getOtherLight().setremaininglighttime(c.getCurrentLight().getOtherLight().getYellowTime()+newtime);
+				}
+				
+				int k=0;
+				int carstomove=(config.carspeed+config.carspacing)/(config.carsize+config.carspacing);
+				for(int i=0;i<middlelane.size();i++)
+				{
+					Car car=currentlight.getMiddleLane().get(0);
+					
+					if(car.getState()==0)
+					{
+						
+						if( car.getNextLightIndex()==car.path.size() ||(car.getCurrentLane()==Lane.middle && car.path.get(car.getNextLightIndex()).getMiddleLaneSize()<=car.getCurrentLight().getLaneLimit()) || ( car.getCurrentLane()!=Lane.middle &&(car.path.get(car.getNextLightIndex()).getNumberOfTurningCars()<=car.getCurrentLight().getLaneLimit())))
+						{	 
+						int check=car.getCurrentLight().removecar(car,currenttime+k*(1/carstomove)); // We removed car and added it to appropriate traffic light.
+						// Now we need to create new carupdate event.
+						car.moving(); // Car is in motion.
+						if(check==1)
+							futureEvents.add(car.generateCarUpdateEvent(currenttime+k*(1/carstomove)));
+						}
+					}
+					k++;
+				}
+			//We need to loop through cars in middle lane and forward them.
+			
+			}
+			
+			else
+			{
+				if( c.getNextLightIndex()==c.path.size() ||(c.getCurrentLane()==Lane.middle && c.path.get(c.getNextLightIndex()).getMiddleLaneSize()<=c.getCurrentLight().getLaneLimit()) || ( c.getCurrentLane()!=Lane.middle &&(c.path.get(c.getNextLightIndex()).getNumberOfTurningCars()<=c.getCurrentLight().getLaneLimit())))
+				{	 
+				int check=c.getCurrentLight().removecar(c,currenttime); // We removed car and added it to appropriate traffic light.
+				// Now we need to create new carupdate event.
+				c.moving(); // Car is in motion.
+				if(check==1)
+					futureEvents.add(c.generateCarUpdateEvent(currenttime));
+				}
+			}
 			//}
 		}
 		
-		// BJ: What is this doing?
-		// BJ: The TrafficLights should be controlled by the TrafficLightScheduler!
-		if((currentlight.getCounter() > currentlight.getThreshold()))
+		//Self-managed scheduling
+		if((currentlight.getCounter()>currentlight.getThreshold()))
 		{
-			if(currentlight.getFlag() == false &&  /* BJ: What is this flag? */
-					currentlight.getCurrentLightColor() != LightColor.green)
+			if(currentlight.getFlag()==false && currentlight.getCurrentLightColor()!=LightColor.green)
 			{
 			// Create traffic light update event with current time.
 			//	System.out.println("Creating traffic light update event and number of cars at that light are :"+currentlight.getCounter());
@@ -115,12 +146,15 @@ public class TrafficGrid  {
 			//	System.out.println("Creating traffic light update event and number of cars at that light are :"+currentlight.getCounter());
 				futureEvents.add(new LightEvent(currentlight, currenttime));
 			// By the current event creation if previous traffic light update event of other light is executed then we need to remove that event from eventqueue.
-				// BJ: NO! You should not be deciding that an Event won't be executed after it was placed in the EventQueue
 				eq.remove(currentlight.getOtherLightEvent()); //Remove event if it is present.
 			}
+			
+			
 		}
 		//Handle carupdate
+		
 		return futureEvents;
 	}
+	
 	
 }
